2ebaad418f9672890ec09fb455a479400226afcb
Lehtimies <lofman.johnny@gmail.com>
2024-12-04 03:14:05 +0200

    Added text animation

--- albinGPT.js
+++ albinGPT.js
@@ -1,8 +1,16 @@
 function toggleSidebar() {
-    //document.getElementById('sidebar').style.width='0%'
     const sidebar = document.getElementById('sidebar');
     const openButton = document.getElementById('openSidebarBtn');
 
     sidebar.classList.toggle('sidebar-hidden');
     openButton.classList.toggle('hidden');
+}
+
+function typeOutText(elementID, text, speed) {
+    const element = document.getElementById(elementID);
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            element.innerHTML += text[i];
+        }, speed * i);
+    }
 }
